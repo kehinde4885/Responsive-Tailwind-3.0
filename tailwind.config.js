15174ece6332df708d322854673c04b20581c362
Kehinde Malagu <Kehindemalagu@gmail.com>
2022-11-22 10:48:58 +0100

    First Page in Progress

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,27 @@ module.exports = {
       fontFamily: {
         playfair: "playfair Display ,serif",
         thiccboi: "Thiccboi"
-      }
-    },
+      },
+
+      backgroundImage: {
+        'vid1' : "url('./Images/poster-00001.jpg')",
+        'vid2' : "url('./Images/poster-00002.jpg')",
+        'vid3' : "url('./Images/poster-00003.jpg')",
+        'vid4' : "url('./Images/poster-00004.jpg')",
+        'vid5' : "url('./Images/poster-00005.jpg')",
+        'vid6' : "url('./Images/poster-00006.jpg')",
+        'vid7' : "url('./Images/poster-00007.jpg')",
+        'vid8' : "url('./Images/poster-00008.jpg')",
+        'vid9' : "url('./Images/poster-00009.jpg')",
+        'vid10' : "url('./Images/poster-00010.jpg')",
+      },
+
+      borderRadius: {
+        '4xl': '2rem',
+        '5xl': '2.5rem',
+        '6xl': '3rem'
+       },
   },
   plugins: [],
+}
 }
